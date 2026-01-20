7e61f0ea388217a4a47c9ae2e943ea720d145267
KirillBorisenko-git <borisenko.kirill2019@gmail.com>
2026-01-20 14:21:03 +0500

    first

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className="logo">3dShop</span>
+            <div className="presentation"></div>
+        </div>
+    </header>
+  )
+}
